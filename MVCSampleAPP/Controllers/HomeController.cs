ca318f613abdd890f2da25abbae3f766cb997926
Siva Sankara Medisetty <SMedisetty@agility.com>
2023-03-20 21:59:16 +0530

    Views added

--- MVCSampleAPP/Controllers/HomeController.cs
+++ MVCSampleAPP/Controllers/HomeController.cs
@@ -13,12 +13,54 @@ namespace MVCSampleAPP.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        // GET: Home
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+
+        public ViewResult ForgotPassword()
+        {
+            var name = "raju";
+
+            if (name == "raju")
+            {
+                return View("ForgotPwd");
+
+            }
+            else
+            {
+                return View("~/Views/Home/ResetPwd.cshtml");
+            }
+        }
+
+        public ViewResult ResetPassword()
+        {
+            return View("~/Views/Home/ResetPwd.cshtml");
+        }
+
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+        public ViewResult Mission()
+        {
+            return View("~/Views/Test/Mission.cshtml");
+        }
+
+        public ViewResult About()
         {
             return View();
         }
